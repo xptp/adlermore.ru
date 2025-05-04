07ea5f091e28c579968d9b4e89679b2ffb2a9c4c
Ya <nesquik1723@gmail.com>
2025-05-04 12:26:14 +0300

    galleryimgwebp

--- src/app/components/ui/gallery.jsx
+++ src/app/components/ui/gallery.jsx
@@ -5,11 +5,11 @@ const Gallery = () => {
     <div className="gallery-container" id="gallery-section">
       <h2 className="h2-gallery ">Галлерея</h2>
       <div className="gallery-img-block">
-        <img src="/img/gallery/1.png" alt="" />
+        <img src="/img/gallery/1.webp" alt="" />
         <img src="/img/gallery/3.webp" alt="" />
         <img src="/img/gallery/5.webp" alt="" />
         <img src="/img/gallery/6.webp" alt="" />
-        <img src="/img/gallery/21.jpg" alt="" />
+        <img src="/img/gallery/21.webp" alt="" />
         {/* <img src="https://hotel-blacksea.com/img/galleryfull22.jpg" alt="" />
         <img src="https://hotel-blacksea.com/img/galleryfull39.jpg" alt="" />
         <img src="https://hotel-blacksea.com/img/galleryfull15.jpg" alt="" /> */}
