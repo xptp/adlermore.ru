d3111975f17477bb963df05056658af1454ed71e
Ya <nesquik1723@gmail.com>
2025-06-11 11:57:36 +0300

    video

--- src/app/components/ui/bookingImg.jsx
+++ src/app/components/ui/bookingImg.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import HotelWidget from "./hotelBookingWidget";
 import "../../styles/ui/imgBlock.scss";
+// import video from "./video/mainVideo.mp4";
 // import { useWindowWidth } from "../hooks/useWindowWidth";
 const BookingImg = () => {
   // const width = useWindowWidth();
   return (
     <div className="img-block">
+      <video autoPlay muted loop playsInline className="bg-video">
+        <source src="/video/mainVideo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="img-block-content">
         <div className="img_text">Рады гостям круглый год!</div>
         <div className="wiget-img">
@@ -17,3 +22,9 @@ const BookingImg = () => {
 };
 
 export default BookingImg;
+
+{
+  /* <video autoPlay muted loop playsInline>
+        <source src="/video/mainVideo.mp4" type="video/mp4" />
+      </video> */
+}
