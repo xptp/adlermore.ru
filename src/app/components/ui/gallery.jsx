657ffaf41acf45a17dd49edf1a57ee83fff82b6f
Ya <nesquik1723@gmail.com>
2025-07-05 11:03:26 +0300

    photo

--- src/app/components/ui/gallery.jsx
+++ src/app/components/ui/gallery.jsx
@@ -5,20 +5,21 @@ const Gallery = () => {
     <div className="gallery-container" id="gallery-section">
       <h2 className="h2-gallery ">Галерея</h2>
       <div className="gallery-img-block">
+        <img src="/img/gallery/1.webp" alt="Фото крыша,шезлонги" />
+        <img src="/img/gallery/2.webp" alt="Фото крыша,шезлонги" />
         <img
-          src="/img/gallery/1.webp"
-          alt="Фото цветов в номере отеля или гостиницы"
-        />
-        <img src="/img/gallery/3.webp" alt="Фото на крыше отеля с бассейном" />
-        <img
-          src="/img/gallery/5.webp"
+          src="/img/gallery/3.webp"
           alt="Бассейн на крыше отеля или гостиницы"
         />
-        <img src="/img/gallery/6.webp" alt="Номер отеля или гостиницы" />
+        <img src="/img/gallery/4.webp" alt="Крыша отеля с бассейном" />
         <img
-          src="/img/gallery/21.webp"
+          src="/img/gallery/5.webp"
           alt="Крыша отеля с шезлонгом для отдыха"
         />
+        <img src="/img/gallery/6.webp" alt="Вид с крыши отеля на Адлер" />
+        <img src="/img/gallery/7.webp" alt="Шезлонг на крыше отеля" />
+        <img src="/img/gallery/8.webp" alt="Шезлонг на крыше отеля" />
+        <img src="/img/gallery/9.webp" alt="Цветы в номере отеля" />
         {/* <img src="https://hotel-blacksea.com/img/galleryfull22.jpg" alt="" />
         <img src="https://hotel-blacksea.com/img/galleryfull39.jpg" alt="" />
         <img src="https://hotel-blacksea.com/img/galleryfull15.jpg" alt="" /> */}
